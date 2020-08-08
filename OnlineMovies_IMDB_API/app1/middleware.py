df1665831c69f94ebc95ea2b8cb59c454efa6cc5
Bhagath Kola <52624055+BhagathKola@users.noreply.github.com>
2020-08-08 09:21:57 +0530

    second commit

--- OnlineMovies_IMDB_API/app1/middleware.py
+++ OnlineMovies_IMDB_API/app1/middleware.py
@@ -1,51 +1,22 @@
+import http.client
 import json
-from OnlineMovies_IMDB_API.settings import MOVIES_API_FILE
 
-class MovieMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-        t_list=fetchMovie_id()
-        import requests
-        list_of_data=[]
-        for title in t_list:
+class Moviesmiddleware:
+    def __init__(self,get_response):
+        self.get_response=get_response
+        conn = http.client.HTTPSConnection("imdb8.p.rapidapi.com")
+        headers = {
+                    'x-rapidapi-host': "imdb8.p.rapidapi.com",
+                    'x-rapidapi-key': "4422ecdd22msh0284eaac7d64721p1d830ejsn5fc033392fd1",
+                  }
+        conn.request("GET", "/title/auto-complete?q=game%20of%20thr", headers=headers)
+        res = conn.getresponse()
+        data = res.read()
+        x=data.decode("utf-8")
+        dict_data = json.loads(x)
+        # print(dict_data)
+        json.dump(dict_data,open("app1/raw/package.json","w"))
 
-            url = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/"+title
-
-            headers = {
-                'x-rapidapi-host': "imdb-internet-movie-database-unofficial.p.rapidapi.com",
-                'x-rapidapi-key': "0ac662495cmsha3ddbe01a85b038p1b0e32jsn0541b2f89aff"
-            }
-
-            response = requests.request("GET", url, headers=headers)
-
-            dict_data = json.loads(response.text)
-            list_of_data.append(dict_data)
-        with open(MOVIES_API_FILE, 'w') as file:
-            print(list_of_data)
-            json.dump(list_of_data, file, indent=2)
-
-    def __call__(self, request, *args, **kwargs):
-        response = self.get_response(request)
+    def __call__(self,request, *args, **kwargs):
+        response=self.get_response(request)
         return response
-
-def fetchMovie_id():
-    import requests
-
-    url = "https://imdb8.p.rapidapi.com/title/get-popular-movies-by-genre"
-
-    querystring = {"genre": "%2Fchart%2Fpopular%2Fgenre%2Fadventure"}
-
-    headers = {
-        'x-rapidapi-host': "imdb8.p.rapidapi.com",
-        'x-rapidapi-key': "0ac662495cmsha3ddbe01a85b038p1b0e32jsn0541b2f89aff"
-    }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    resstr=response.text.split('/')
-    print(type(resstr))
-    l=[]
-    for x in resstr:
-        if x.split('/'):
-            if x.startswith('tt'):
-                l.append(x)
-    return l
